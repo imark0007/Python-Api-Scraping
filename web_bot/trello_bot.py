915ddf9e7de586ce6098c4ccad4fde1e21739b16
Arka Talukder <arkatalukder99@gmail.com>
2024-01-10 02:02:22 +0600

    initial

---
+++ web_bot/trello_bot.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+import time
+from datetime import date
+import os
+import json
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+    
