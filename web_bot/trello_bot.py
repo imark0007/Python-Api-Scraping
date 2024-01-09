c5b6f44574ba90ee065aba1f707d3de3952fac40
Arka Talukder <arkatalukder99@gmail.com>
2024-01-10 02:40:47 +0600

    initial

--- web_bot/trello_bot.py
+++ web_bot/trello_bot.py
@@ -7,8 +7,18 @@ from datetime import date
 import os
 import json
 
+CHROME_DRIVER_PATH = os.path.join(os.getcwd(), "chromedriver")
+OP = webdriver.ChromeOptions()
+OP.add_argument('--headless')
+DRIVER = webdriver.Chrome(CHROME_DRIVER_PATH)
+
+
+
 def main():
-    pass
+    try:
+        DRIVER.get("https://trello.com")
+    except Exception as e:
+        print(e)
 
 if __name__ == "__main__":
     main()
